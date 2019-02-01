5a1caaa8d6172740f023a21c9cdce3de7460692d
risheric <risheric@gmail.com>
2019-02-01 17:27:59 -0300

    added test for dict_to_namedtuple

--- test/utils_test.py
+++ test/utils_test.py
@@ -1,6 +1,7 @@
 from muttlib import utils
 import datetime
 import pytest
+from collections import namedtuple
 
 def test_str_to_datetime():
     assert utils.str_to_datetime('2019-10-25 18:35:22') == datetime.datetime(2019, 10, 25, 18, 35, 22)
@@ -16,4 +17,7 @@ def test_str_to_datetime():
     with pytest.raises(ValueError):
         utils.str_to_datetime("25/10/2019")
 
-
+def test_dict_to_namedtuple():
+    tuple_1 = namedtuple('pepe','don_jose') ('paso por mi casa') 
+    tuple_2 = utils.dict_to_namedtuple('pepe',{'don_jose': 'paso por mi casa'})
+    assert tuple_1 == tuple_2
