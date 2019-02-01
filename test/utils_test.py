8d953469d89a9ef6964f543854e04952d3bb512a
risheric <risheric@gmail.com>
2019-02-01 16:27:31 -0300

    added logic for exception

--- test/utils_test.py
+++ test/utils_test.py
@@ -1,5 +1,10 @@
 from muttlib import utils
 import datetime
+import pytest
 
 def test_str_to_datetime():
 	assert utils.str_to_datetime("2019-10-25") == datetime.datetime(2019,10,25)
+	with pytest.raises(ValueError):
+		utils.str_to_datetime("25/10/2019")
+
+
