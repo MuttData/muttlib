0516489f91ea98d8c0de28de8f45339cb90b207e
jdemonasterio <mateo@muttdata.ai>
2019-07-31 01:28:00 -0300

    en mass test of all timeRangeConf is-time-gran methods

--- tests/test_gcd.py
+++ tests/test_gcd.py
@@ -19,6 +19,12 @@ def test_no_value():
         TimeRangeConfiguration()  # pylint: disable=no-value-for-parameter
 
 
+def assert_not_all_except_true_one_attr(obj, attr_l, this_attr):
+    assert getattr(obj, this_attr)()
+    for a in [a for a in attr_l if a != this_attr]:
+        assert not getattr(obj, a)()
+
+
 @pytest.mark.parametrize(
     "ed, twin, fwin, tg, ehour, sd, fd, tn",
     [
@@ -42,6 +48,16 @@ def test_no_value():
             pd.to_datetime('2019-07-31'),
             'monthly',
         ),
+        (
+            pd.to_datetime('2019-06-01'),
+            30,
+            15,
+            'D',
+            0,
+            pd.Timestamp("'2019-05-02"),
+            pd.to_datetime('2019-06-16'),
+            'daily',
+        ),
     ],
 )
 def test_time_range_configuration_init(ed, twin, fwin, tg, ehour, sd, fd, tn):
@@ -51,6 +67,15 @@ def test_time_range_configuration_init(ed, twin, fwin, tg, ehour, sd, fd, tn):
     assert trc.future_date == fd
     assert trc.time_granularity == tg
     assert trc.time_granularity_name == tn
+    time_gran_methods = ["is_hourly", "is_daily", "is_weekly", "is_monthly"]
+    if tg == 'H':
+        assert_not_all_except_true_one_attr(trc, time_gran_methods, 'is_hourly')
+    elif tg == 'D':
+        assert_not_all_except_true_one_attr(trc, time_gran_methods, 'is_daily')
+    elif tg == 'W':
+        assert_not_all_except_true_one_attr(trc, time_gran_methods, 'is_weekly')
+    elif tg == 'M':
+        assert_not_all_except_true_one_attr(trc, time_gran_methods, 'is_monthly')
 
 
 @pytest.mark.parametrize(
