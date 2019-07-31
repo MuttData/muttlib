2c59fcf059fb040b33b368e24543c208d45a393b
jdemonasterio <mateo@muttdata.ai>
2019-07-31 01:13:23 -0300

    multiple bad initialization parametrize

--- tests/test_gcd.py
+++ tests/test_gcd.py
@@ -53,6 +53,19 @@ def test_time_range_configuration_init(ed, twin, fwin, tg, ehour, sd, fd, tn):
     assert trc.time_granularity_name == tn
 
 
-def test_time_range_configuration_init_error():
-    with pytest.raises(ValueError):
-        TimeRangeConfiguration(pd.to_datetime('2019-06-01'), -2, -1, 'H')
+@pytest.mark.parametrize(
+    "ed, twin, fwin, tg, expected_error",
+    [
+        (pd.to_datetime('2019-06-01'), 2, -1, 'H', ValueError),  # bad windows
+        (pd.to_datetime('2019-06-01'), -2, -1, 'H', ValueError),  # bad windows
+        (pd.to_datetime('2019-06-01'), -20, 1, 'H', ValueError),  # bad windows
+        (pd.to_datetime('2019-06-01'), 2, 1, 'J', KeyError),  # bad granularity
+        (pd.to_datetime('2019-06-01'), 2, 1, 'foo', KeyError),  # bad granularity
+        (pd.to_datetime('2019-06-01'), 2, 1, 4, KeyError),  # bad granularity
+        (0, 2, 1, 4, TypeError),  # bad end-date
+        ("foo", 2, 1, 4, ValueError),  # bad end-date
+    ],
+)
+def test_time_range_configuration_init_error(ed, twin, fwin, tg, expected_error):
+    with pytest.raises(expected_error):
+        TimeRangeConfiguration(ed, twin, fwin, tg)
