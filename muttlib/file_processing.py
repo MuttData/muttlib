d978e697965a7f3f25cee3a038259abd13a6d60c
Pablo Lorenzatto <pablo.a.lorenzatto@gmail.com>
2020-10-21 02:19:40 -0300

    Added tests to file_processing.py

--- muttlib/file_processing.py
+++ muttlib/file_processing.py
@@ -1,22 +1,24 @@
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import Future, ProcessPoolExecutor
+import functools
 import glob
+import inspect
 import logging
-from multiprocessing import cpu_count
+from multiprocessing import Process, Queue, cpu_count
 import os
+from pathlib import Path
 import time
 
+READY_PREFIX = "ready"
+DONE_PREFIX = "done"
+FAIL_PREFIX = "fail"
 
-READY_PREFIX = 'ready'
-DONE_PREFIX = 'done'
-FAIL_PREFIX = 'fail'
+STAGING_PREFIX = "stg"
 
-STAGING_PREFIX = 'stg'
+logger = logging.getLogger(__name__)
 
 
 class FileNotReady(Exception):
     """Should be raised when filename does not start with READY_PREFIX."""
-
-    pass
 
 
 class RenameByResult:
@@ -27,102 +29,108 @@ class RenameByResult:
 
     def __init__(
         self,
-        fn,
+        fn="",
+        fn_arg=None,
+        ok_unless_exception=True,
         ready_prefix=READY_PREFIX,
         done_prefix=DONE_PREFIX,
         fail_prefix=FAIL_PREFIX,
     ):
-        """Setup."""
-        self.fn = fn
+        """Setup instance to work as either contextmanager or decorator.
+
+        Either fn or decorate_arg mut be provided
+
+        Args:
+            fn (str, Path): Directory from which to read files.
+            fn_arg (str): When used as decorator this name of the argument from which the path will
+                          be taken.
+            ready_prefix (str): Preffix to check if the file is ready.
+            done_prefix (str): Preffix to set if call result was successful.
+            fail_prefix (str): Preffix to set if call failed.
+        """
+        if not (fn or fn_arg):
+            raise ValueError("Either fn or decorate_arg mut be provided.")
+        self.fn = Path(fn)
+        self.fn_arg = fn_arg
+        self.ok_unless_exception = ok_unless_exception
         self.ready_prefix = ready_prefix
         self.done_prefix = done_prefix
         self.fail_prefix = fail_prefix
         self.result = None
-        self.base_fn = os.path.basename(self.fn)
+        self._t0 = None
+
+    @property
+    def clean_fn(self):
+        rv = self.fn.name
+        if self.fn.name.startswith(self.ready_prefix):
+            rv = rv[len(self.ready_prefix) + 1 :]
+        return rv
+
+    def _prefixed_fn(self, prefix):
+        return self.fn.parent / f"{prefix}_{self.clean_fn}"
+
+    @property
+    def done_fn(self):
+        return self._prefixed_fn(self.done_prefix)
+
+    @property
+    def fail_fn(self):
+        return self._prefixed_fn(self.fail_prefix)
 
     def __enter__(self):
         """Ensure that the given file is ready."""
-        logging.info(f"Processing {self.fn}")
-        if not self.base_fn.startswith(self.ready_prefix):
-            self.fail_fn = f'{self.fail_prefix}_{self.base_fn}'
-            raise FileNotReady()
-        else:
-            self.clean_fn = self.base_fn[len(self.ready_prefix) + 1 :]
-        self.done_fn = self.done_prefix + self.base_fn[len(self.ready_prefix) :]
-        self.fail_fn = self.fail_prefix + self.base_fn[len(self.ready_prefix) :]
-        self.t0 = time.time()
+        logger.info(f"Processing {self.fn}")
+        self._t0 = time.time()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """Rename input file based on result."""
-        if self.result:
+        if self.result or (self.ok_unless_exception and not exc_val):
             result_fn = self.done_fn
-            logging.info(f"DONE ({round(time.time() - self.t0, 3)}s): {self.fn}")
+            logger.info(f"DONE ({round(time.time() - self._t0, 3)}s): {self.fn}")
         else:
             result_fn = self.fail_fn
             if exc_val:
-                logging.error(f"ERROR: {self.fn}", exc_info=(exc_type, exc_val, exc_tb))
+                logger.error(f"ERROR: {self.fn}", exc_info=(exc_type, exc_val, exc_tb))
             else:
-                logging.warning(f"FAIL: {self.fn}")
+                logger.warning(f"FAIL: {self.fn}")
 
-        result_fn = os.path.join(os.path.dirname(self.fn), result_fn)
-        os.rename(self.fn, result_fn)
+        self.fn.rename(result_fn)
 
     def set_result(self, result):
         """Set attribute to indicate if task was successfull.
 
-        Note that in some cases and empty result (such as [] or {}) can be
-        valid but it will be converted into False and the task for this file
-        will be considered as failed.
-        In such cases return smth like {None: []} that can be easily
-        discarded down the line.
+        Note that in some cases and empty result (such as [] or {}) can
+        be valid but it will be converted into False and the task for
+        this file will be considered as failed. In such cases return
+        smth like {None: []} that can be easily discarded down the line.
         """
         self.result = bool(result)
 
+    def __call__(self, func):
+        sig = inspect.signature(func)
+        if self.fn_arg is None:
+            raise ValueError(
+                "The constructor arg fn_arg must be passed when using as decorator."
+            )
+        if self.fn_arg not in sig.parameters:
+            raise ValueError(
+                f"Decorated function doesn't have '{self.fn_arg}' argument."
+            )
 
-def get_new_files(in_dir, only_ready=False, ready_prefix=READY_PREFIX):
-    """Get files ready for processing."""
-    filter_prefix = ready_prefix if only_ready else ''
-    fns = glob.glob(os.path.join(in_dir, filter_prefix + '*'))
-    return fns
+        @functools.wraps(func)
+        def wrapper_decorator(*args, **kwargs):
+            self.fn = Path(sig.bind(*args, **kwargs).arguments[self.fn_arg])
+            with self as rbr:
+                rv = func(*args, **kwargs)
+                rbr.set_result(rv)
+            return rv
 
-
-def process_new_files(proc_func, in_dir, **extra_data):
-    """Process files that match ready prefix.
-
-    Process files that match ready prefix the given function and rename them
-    based on the result.
-    """
-    for fn in get_new_files(in_dir):
-        with RenameByResult(fn) as rbr:
-            res = proc_func(fn, rbr, **extra_data)
-            rbr.set_result(res)
-
-
-def worker_wrapper(proc_func, fn, use_rbr=False, **extra_data):
-    """Wrap proc_func to add file rename."""
-    if use_rbr:
-        with RenameByResult(fn) as rbr:
-            res = proc_func(fn, rbr, **extra_data)
-            rbr.set_result(res)
-    else:
-        res = proc_func(fn, **extra_data)
-    return res
+        return wrapper_decorator
 
 
 class DummyPoolExecutor:
     """Dummy class to emulate multiprocessing."""
-
-    class DummyFuture:
-        """Emulation of concurrent.futures.Future."""
-
-        def __init__(self, result):
-            """Set dummy future result with given arg."""
-            self.result = result
-
-        def result(self):
-            """Emulate concurrent.futures.Future().result."""
-            return self.result
 
     def __init__(self, *args, **kwargs):
         """Keep args and do nothing."""
@@ -135,42 +143,57 @@ class DummyPoolExecutor:
 
     def __exit__(self, *args):
         """Exit ContextManger."""
-        pass
 
     def submit(self, fn, *args, **kwargs):
         """Return dummy future with function return as result."""
-        return self.DummyFuture(fn(*args, **kwargs))
+        future = Future()
+        future.set_result(fn(*args, **kwargs))
+        return future
 
 
-def process_new_files_parallel(
-    proc_func, in_dir, workers=None, only_ready=False, **extra_data
-):
-    """Process files that match ready prefix in parallel.
+class IsolatedProcessFailedBadly(Exception):
+    """Exception raised when the isolated process fails badly."""
 
-    Process files that match ready prefix the given function and rename them
-    based on the result.
-    By default the number of workers is equal to the number of cores.
 
-    TODO:
-      - Add selector for pool type (process or thread). Threads would be the
-        preffered option if proc_func frees the GIL either by using Numba or
-        Cython.
+def isolate_bad_failures(func, enabled=True):
+    """Decorator to isolate code in a separate process.
+
+    This decorator provides a safe way to run code that may fail badly without killing the parent.
+    (Fail badly meaning be killed via SIGKILL or suffer segmentation faults).
+
+    If the function runs inside a process based execution pool consider using theads since this
+    decorator when (enabled) already solves the GIL issue.
+
+    Args:
+        func (function): Function to decorate.
+        enabled (bool; True): Enable or disable the isolation.
     """
-    if workers is None:
-        workers = cpu_count()
+    if not enabled:
+        return func
 
-    if workers > 0:
-        pool = ProcessPoolExecutor(max_workers=workers)
-        logging.info(f"Using process {workers} workers for file processing.")
-    else:
-        pool = DummyPoolExecutor()
-        logging.info(f"No for file processing.")
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
 
-    futures = []
-    with pool as executor:
-        for fn in get_new_files(in_dir, only_ready):
-            futures.append(executor.submit(worker_wrapper, proc_func, fn, **extra_data))
+        q = Queue()
 
-    res = [f.result() for f in futures]
+        def h(q, func, *args, **kwargs):
+            try:
+                qv = func(*args, **kwargs)
+            except Exception as e:  # pylint:disable=W0703
+                qv = e
+            q.put(qv)
 
-    return res
+        p = Process(target=h, args=(q, func, *args), kwargs=kwargs)
+        p.start()
+        p.join()
+        if q.empty():
+            raise IsolatedProcessFailedBadly(
+                "Process ended badly check for kills and segfaults."
+            )
+        else:
+            res = q.get()
+            if isinstance(res, Exception):
+                raise res
+        return res
+
+    return wrapper_decorator
