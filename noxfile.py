28e8e927be35c91f7fcaba12c3ad53a1ecbb7a63
fabian wolfmann <fabian.wolfmann@alumnos.unc.edu.ar>
2020-10-26 10:35:28 -0300

    add teradata to test session

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ import nox
 def tests(session):
     """Run all tests."""
     session.install(".")
-    session.install(".[test,forecast,gdrive,postgres,mysql,mysql,bigquery]")
+    session.install(".[test,forecast,gdrive,postgres,mysql,mysql,bigquery,teradata]")
 
     cmd = ["pytest", "-n", "auto"]
 
