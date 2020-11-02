5a23489e1b5e63c576fcfd1b846adc5259bd554d
Luis Alberto Hernandez <luis@muttdata.ai>
2020-11-02 14:50:19 +0000

    RFC: Resolve "Install all extras deps on nox tests session"

--- noxfile.py
+++ noxfile.py
@@ -1,29 +1,21 @@
 """Entrypoint for nox."""
-
 import nox
 
 
 @nox.session(reuse_venv=True)
 def tests(session):
     """Run all tests."""
-    session.install(".")
-    session.install(".[test,forecast,gdrive,postgres,mysql,mysql,bigquery,ipynb-utils]")
-
+    session.install(".[all]")
     cmd = ["pytest", "-n", "auto"]
-
     if session.posargs:
         cmd.extend(session.posargs)
-
     session.run(*cmd)
 
 
 @nox.session(reuse_venv=True)
 def cop(session):
     """Run all pre-commit hooks."""
-    session.install(".")
-    session.install(".[dev]")
-    session.install(".[test]")
-
+    session.install(".[dev,test]")
     session.run("pre-commit", "install")
     session.run("pre-commit", "run", "--show-diff-on-failure", "--all-files")
 
@@ -32,5 +24,4 @@ def cop(session):
 def bandit(session):
     """Run all pre-commit hooks."""
     session.install("bandit")
-
     session.run("bandit", "-r", "muttlib/", "-ll", "-c", "bandit.yaml")
