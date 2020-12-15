43e4241624ae8f475525486c8a13f01ccda499bd
Javi M <ijmermet@gmail.com>
2020-12-15 03:33:55 +0000

    [FIX] pipelines and nox

--- noxfile.py
+++ noxfile.py
@@ -2,33 +2,32 @@
 import nox
 
 
-@nox.session(reuse_venv=True)
+@nox.session(reuse_venv=True, python=["3.7", "3.8"])
 def tests(session):
     """Run all tests."""
-    session.install(".[all]")
+    session.install("pip==20.3.1")
+    session.install(".[all]", "--use-deprecated=legacy-resolver")
+
     cmd = ["pytest", "-n", "auto"]
+
     if session.posargs:
         cmd.extend(session.posargs)
+
     session.run(*cmd)
 
 
-@nox.session(reuse_venv=True)
+@nox.session(reuse_venv=True, python=["3.7", "3.8"])
 def cop(session):
     """Run all pre-commit hooks."""
-    session.install(".[all]")
-    session.run("pre-commit", "install", "-c", ".pre-commit-config-nox.yaml")
-    session.run(
-        "pre-commit",
-        "run",
-        "--show-diff-on-failure",
-        "--all-files",
-        "-c",
-        ".pre-commit-config-nox.yaml",
-    )
+    session.install("pip==20.3.1")
+    session.install(".[all]", "--use-deprecated=legacy-resolver")
+    session.run("pre-commit", "install")
+    session.run("pre-commit", "run", "--show-diff-on-failure", "--all-files")
 
 
-@nox.session(reuse_venv=True)
+@nox.session(reuse_venv=True, python=["3.7", "3.8"])
 def bandit(session):
     """Run all pre-commit hooks."""
-    session.install("bandit")
+    session.install("pip==20.3.1")
+    session.install("bandit", "--use-deprecated=legacy-resolver")
     session.run("bandit", "-r", "muttlib/", "-ll", "-c", "bandit.yaml")
