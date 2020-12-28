46265b3d172f1264318eea1a8d66427d3eb2cbd6
wenceslao villegas <wences@muttdata.ai>
2020-12-28 15:15:45 -0300

    lint

--- muttlib/dbconn/bigquery.py
+++ muttlib/dbconn/bigquery.py
@@ -53,9 +53,7 @@ class BigQueryClient(BaseClient):
         self.table = table
 
     def _read_cred(
-        self,
-        auth: Optional[str],
-        auth_file: Optional[Union[str, Path]],
+        self, auth: Optional[str], auth_file: Optional[Union[str, Path]],
     ):
         """Create valid OAuth2 credentials for bigquery.
 
