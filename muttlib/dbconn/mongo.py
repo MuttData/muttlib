ec4289dadecfb3299811f045d807bf8e0210762c
wenceslao villegas <wences@muttdata.ai>
2021-01-05 21:20:05 -0300

    Reverted mongo changes

--- muttlib/dbconn/mongo.py
+++ muttlib/dbconn/mongo.py
@@ -3,7 +3,7 @@ import logging
 from pandas.io.json import json_normalize
 
 logger = logging.getLogger(__name__)
-from muttlib.dbconn.base import ClientBaseClient
+
 
 try:
     import pymongo
@@ -13,7 +13,7 @@ except ModuleNotFoundError:
 MONGO_DB_TYPE = 'mongo'
 
 
-class MongoClient(ClientBaseClient):
+class MongoClient:
     """MongoDb client."""
 
     def __init__(
@@ -26,19 +26,15 @@ class MongoClient(ClientBaseClient):
         seeds=None,
         replica_set=None,
     ):
-        super().__init__(
-            username=username,
-            password=password,
-            database=database,
-            host=host,
-            port=port,
-        )
         if host is None and seeds is None:
             raise ValueError("Either `host` or `seeds` is required and was not found!")
-
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
         self.seeds = seeds
         self.replica_set = replica_set
-        self.conn_url = self._build_conn_uri()
 
     def _build_conn_uri(self):
         uri = 'mongodb://'
@@ -54,18 +50,13 @@ class MongoClient(ClientBaseClient):
         uri += f'?replicaSet={self.replica_set}' if self.replica_set is not None else ''
         return uri
 
-    def _connect(self):
-        """Get Database of a MongoClient.
-
-        Returns
-        -------
-        pymongo.database.Database
-        """
-        client = pymongo.MongoClient(self.conn_str)
+    def _connect(self, custom_uri=None):
+        if custom_uri is None:
+            conn_uri = self._build_conn_uri()
+        client = pymongo.MongoClient(conn_uri)
         return client[self.database]
 
     def _find(self, collection, query=None, fields=None, db=None, limit=0):
-        """Execute find operation on collection."""
         if db is None:
             db = self._connect()
         collection = db[collection]
@@ -90,11 +81,3 @@ class MongoClient(ClientBaseClient):
         collection = db[collection]
         insert_id = collection.insert_one(query).inserted_id
         return insert_id
-
-    def execute(self, collection, action, query=None, **kwargs):
-        """Execute query."""
-        return NotImplementedError
-
-    def insert_from_frame(self, df, collection, **kwargs):
-        """Insert from a Pandas dataframe."""
-        return NotImplementedError
