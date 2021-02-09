865b341cd0f6c837bb18a9cb4d7a5e3b2d6076ca
Guido Trucco <guido@muttdata.ai>
2021-02-09 15:31:22 -0300

    test init without host

--- tests/dbconn/test_hive.py
+++ tests/dbconn/test_hive.py
@@ -17,6 +17,12 @@ def dummy_db_credentials():
     }
 
 
+def test_init_without_host_fails(dummy_db_credentials):
+    dummy_db_credentials.pop("host")
+    with pytest.raises(TypeError):
+        HiveClient(**dummy_db_credentials)
+
+
 def test_execute(dummy_db_credentials):
     with patch("pyhive.hive.Connection") as connection:
         hive_cli = HiveClient(**dummy_db_credentials)
