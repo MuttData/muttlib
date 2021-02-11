16e38d02dc288c0b13f5a7df9ab7af5a09587857
Guido Trucco <guido@muttdata.ai>
2021-02-11 14:35:25 -0300

    assert connection

--- tests/dbconn/test_ibis.py
+++ tests/dbconn/test_ibis.py
@@ -22,3 +22,20 @@ def test_temp_hdfs_path_changes_config():
         temp_hdfs_path = MagicMock()
         ibis_cli = IbisClient("host", temp_hdfs_path=temp_hdfs_path)
         config.set_option.assert_called_once_with("temp_hdfs_path", temp_hdfs_path)
+
+
+def test_execute_with_no_client_establishes_connection():
+    with patch("ibis.impala") as impala:
+        ibis_cli = IbisClient("host")
+        q = "SELECT *"
+        ibis_cli.execute_new(q)
+        impala.connect.assert_called_once()
+
+
+def test_execute_with_client_does_not_establish_connection():
+    with patch("ibis.impala") as impala:
+        ibis_cli = IbisClient("host")
+        client = MagicMock()
+        q = "SELECT *"
+        ibis_cli.execute_new(q, client=client)
+        impala.connect.assert_not_called()
