474cf1a7e6ff353ffd63fd567f3cfde6c80ca4b4
Guido Trucco <guido@muttdata.ai>
2021-02-17 15:43:18 -0300

    suggested changes

--- tests/dbconn/test_ibis.py
+++ tests/dbconn/test_ibis.py
@@ -7,6 +7,15 @@ import urllib.parse
 from muttlib.dbconn import IbisClient
 
 
+def call_to_frame_with_cache_dir(q, local_tmp_table_dir, **to_frame_kwargs):
+    with patch("muttlib.utils.hash_str", return_value="123") as hashed_q:
+        cache_dir = MagicMock()
+        cache_dir.__truediv__.return_value = local_tmp_table_dir
+        ibis_cli = IbisClient("host", hdfs_host="", hdfs_port="", hdfs_username="")
+        ibis_cli.to_frame(q, via_hdfs=True, cache_dir=cache_dir, **to_frame_kwargs)
+        cache_dir.__truediv__.assert_called_once_with("ibis_tmp_123")
+
+
 def test_init_without_host_fails():
     with pytest.raises(TypeError):
         IbisClient()
@@ -60,6 +69,7 @@ def test_to_frame_via_hdfs_and_no_hdfs_client_fails():
         q = "SELECT *"
         with pytest.raises(ValueError, match=r".*hdfs\Wclient.*"):
             ibis_cli.to_frame(q, via_hdfs=True)
+        impala.connect.assert_called_once()
 
 
 def test_to_frame_via_hdfs_and_no_cache_dir_fails():
@@ -68,6 +78,7 @@ def test_to_frame_via_hdfs_and_no_cache_dir_fails():
         q = "SELECT *"
         with pytest.raises(ValueError, match=r".*dir.*"):
             ibis_cli.to_frame(q, via_hdfs=True)
+        impala.connect.assert_called_once()
 
 
 def test_to_frame_via_hdfs_and_refresh_cache():
@@ -76,7 +87,10 @@ def test_to_frame_via_hdfs_and_refresh_cache():
     ) as pq:
         ibis_cli = IbisClient("host", hdfs_host="", hdfs_port="", hdfs_username="")
         q = "SELECT *"
-        ibis_cli.to_frame(q, via_hdfs=True, cache_dir=MagicMock(), refresh_cache=True)
+        call_to_frame_with_cache_dir(
+            q, local_tmp_table_dir=MagicMock(), refresh_cache=True
+        )
+        impala.connect.assert_called_once()
         rm.assert_called_once()
 
 
@@ -89,11 +103,9 @@ def test_to_frame_via_hdfs_creates_tmp_table():
         local_tmp_table_dir = MagicMock()
         local_tmp_table_dir.exists.return_value = False
         local_tmp_table_dir.glob.return_value = [MagicMock(return_value=True)]
-        cache_dir = MagicMock()
-        cache_dir.__truediv__.return_value = local_tmp_table_dir
-        ibis_cli = IbisClient("host", hdfs_host="", hdfs_port="", hdfs_username="")
         q = "example query"
-        ibis_cli.to_frame(q, via_hdfs=True, cache_dir=cache_dir)
+        call_to_frame_with_cache_dir(q, local_tmp_table_dir)
+        impala.connect.assert_called_once()
         queries = [x[0][0] for x in impala.connect.return_value.raw_sql.call_args_list]
         # assert all queries were made to ibis_tmp
         assert all("ibis_tmp" in q for q in queries)
@@ -107,18 +119,24 @@ def test_to_frame_via_hdfs_creates_tmp_table():
 
 
 def test_to_frame_via_hdfs_create_tmp_table_fails():
-    with patch("ibis.impala") as impala, patch("muttlib.dbconn.ibis.urlparse") as parse:
+    with patch("ibis.impala") as impala, patch(
+        "muttlib.dbconn.ibis.sleep"
+    ) as sleep, patch("muttlib.dbconn.ibis.urlparse") as parse, patch(
+        "muttlib.utils.make_dirs"
+    ) as make_dirs:
+        impala.connect.return_value.hdfs.get.side_effect = Exception()
         local_tmp_table_dir = MagicMock()
         local_tmp_table_dir.exists.return_value = False
-        cache_dir = MagicMock()
-        cache_dir.__truediv__.return_value = local_tmp_table_dir
-        ibis_cli = IbisClient("host", hdfs_host="", hdfs_port="", hdfs_username="")
+        # cache_dir = MagicMock()
+        # cache_dir.__truediv__.return_value = local_tmp_table_dir
+        # ibis_cli = IbisClient("host", hdfs_host="", hdfs_port="", hdfs_username="")
         q = "example query"
-        impala.connect.return_value.hdfs.get.side_effect = Exception()
-        with pytest.raises(ValueError, match=r".*HDFS.*"):
-            ibis_cli.to_frame(q, via_hdfs=True, cache_dir=cache_dir)
-            # assert retries
-            assert impala.connect.return_value.hdfs.get.call_count > 1
+        with pytest.raises(ValueError, match=r".*HDFS.*") as e:
+            # ibis_cli.to_frame(q, via_hdfs=True, cache_dir=cache_dir)
+            call_to_frame_with_cache_dir(q, local_tmp_table_dir)
+        # assert retries
+        impala.connect.assert_called_once()
+        assert impala.connect.return_value.hdfs.get.call_count > 1
 
 
 def test_to_frame_no_hdfs_returns_empty_df():
