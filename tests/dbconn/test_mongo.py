ecea4a721708efdd297f4ce9337f81f576f9b6d0
Guido Trucco <guido@muttdata.ai>
2021-02-22 15:45:29 -0300

    test_to_frame_with_invalid_limit

--- tests/dbconn/test_mongo.py
+++ tests/dbconn/test_mongo.py
@@ -111,3 +111,19 @@ def test_to_frame_with_negative_limit():
         mongo_cli.to_frame(collection, limit=limit)
         db = cli_mock.return_value.__getitem__.return_value
         db.__getitem__.return_value.find.return_value.limit.assert_not_called()
+
+
+def test_to_frame_with_invalid_limit():
+    with patch("pymongo.MongoClient") as cli_mock:
+        params = {
+            "username": "mutt",
+            "password": "data",
+            "host": "example_host",
+            "database": "example_db",
+        }
+        limit = "not a number"
+        mongo_cli = MongoClient(**params)
+        collection = MagicMock()
+        mongo_cli.to_frame(collection, limit=limit)
+        db = cli_mock.return_value.__getitem__.return_value
+        db.__getitem__.return_value.find.return_value.limit.assert_not_called()
