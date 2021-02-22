e61170052fd6755f7428bfe892b274f7111a7096
Guido Trucco <guido@muttdata.ai>
2021-02-22 12:18:45 -0300

    test_to_frame_uri_with_host_username_and_database

--- tests/dbconn/test_mongo.py
+++ tests/dbconn/test_mongo.py
@@ -25,3 +25,19 @@ def test_to_frame_uri_with_host_username_password_and_database():
             f'mongodb://{params["username"]}:{params["password"]}@{params["host"]}/{params["database"]}'
         )
         cli_mock.return_value.__getitem__.assert_called_once_with(params["database"])
+
+
+def test_to_frame_uri_with_host_username_and_database():
+    with patch("pymongo.MongoClient") as cli_mock:
+        params = {
+            "username": "mutt",
+            "host": "example_host",
+            "database": "example_db",
+        }
+        mongo_cli = MongoClient(**params)
+        collection = MagicMock()
+        mongo_cli.to_frame(collection)
+        cli_mock.assert_called_once_with(
+            f'mongodb://{params["username"]}@{params["host"]}/{params["database"]}'
+        )
+        cli_mock.return_value.__getitem__.assert_called_once_with(params["database"])
