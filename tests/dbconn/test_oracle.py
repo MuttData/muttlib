dc40da080386f011c01653c0a31150d0fbc91c45
Diego Lizondo <lizondo.diego@gmail.com>
2021-02-23 12:59:20 -0300

    Added test test_oracle_insert_from_frame_connects

--- tests/dbconn/test_oracle.py
+++ tests/dbconn/test_oracle.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch, MagicMock, ANY
+
 from muttlib.dbconn import OracleClient
 import pytest
 
@@ -8,7 +10,6 @@ def oracleClient():
         username="scott",
         host="127.0.0.1",
         port=1521,
-        db_type="oracle",
         dialect="oracle",
         password="tiger",
     )
@@ -22,7 +23,6 @@ def oracleClient_withScheme():
         username="scott",
         host="127.0.0.1",
         port=1521,
-        db_type="oracle",
         dialect="oracle",
         password="tiger",
         schema='test',
@@ -44,3 +44,27 @@ def url_connection_with_scheme(oracleClient_withScheme):
 def url_connection_with_scheme_error(oracleClient):
     engine = oracleClient.get_engine()
     assert engine.url != 'oracle://scott:tiger@127.0.0.1:1521/test'
+
+
+def test_oracle_insert_from_frame_connects(oracleClient):
+
+    with patch("muttlib.dbconn.base.create_engine") as create_engine:
+        client = OracleClient(
+            username="scott",
+            host="127.0.0.1",
+            port=1521,
+            dialect="oracle",
+            password="tiger",
+        )
+        df = MagicMock()
+        table = "test_table"
+        client.insert_from_frame(df, table)
+        create_engine.assert_called_once_with(
+            client.conn_str, connect_args=ANY, echo=ANY
+        )
+        df.to_sql.assert_called_once_with(
+            table,
+            create_engine.return_value.connect.return_value.__enter__.return_value,
+            if_exists='append',
+            index=False,
+        )
