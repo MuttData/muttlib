3c33e02fe2261271ab4bce6fb4a0b369ddc904c9
Diego Lizondo <lizondo.diego@gmail.com>
2021-02-24 10:47:49 -0300

    Fixed methods names, and some typos.

--- tests/dbconn/test_oracle.py
+++ tests/dbconn/test_oracle.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.fixture()
-def oracleClient():
+def oracle_client():
     client = OracleClient(
         username="scott",
         host="127.0.0.1",
@@ -18,7 +18,7 @@ def oracleClient():
 
 
 @pytest.fixture(scope='session')
-def oracleClient_withScheme():
+def oracle_client_with_schema():
     client = OracleClient(
         username="scott",
         host="127.0.0.1",
@@ -31,31 +31,31 @@ def oracleClient_withScheme():
     return client
 
 
-def url_connection(oracleClient):
-    engine = oracleClient.get_engine()
+def url_connection(oracle_client):
+    engine = oracle_client.get_engine()
     assert str(engine.url) == 'oracle://scott:tiger@127.0.0.1:1521'
 
 
-def url_connection_with_scheme(oracleClient_withScheme):
-    engine = oracleClient_withScheme.get_engine()
+def url_connection_with_scheme(oracle_client_with_schema):
+    engine = oracle_client_with_schema.get_engine()
     assert str(engine.url) == 'oracle://scott:tiger@127.0.0.1:1521/test'
 
 
-def url_connection_with_scheme_error(oracleClient):
-    engine = oracleClient.get_engine()
+def url_connection_with_scheme_error(oracle_client):
+    engine = oracle_client.get_engine()
     assert engine.url != 'oracle://scott:tiger@127.0.0.1:1521/test'
 
 
-def test_insert_from_frame_connects_fix_club_false(oracleClient):
+def test_insert_from_frame_connects_fix_clob_false(oracle_client):
 
     with patch("muttlib.dbconn.base.create_engine") as create_engine:
 
         df = MagicMock(dtypes='test')
 
         table = "test_table"
-        oracleClient.insert_from_frame(df, table, fix_clobs=False)
+        oracle_client.insert_from_frame(df, table, fix_clobs=False)
         create_engine.assert_called_once_with(
-            oracleClient.conn_str, connect_args=ANY, echo=ANY
+            oracle_client.conn_str, connect_args=ANY, echo=ANY
         )
         df.to_sql.assert_called_once_with(
             table,
@@ -66,16 +66,16 @@ def test_insert_from_frame_connects_fix_club_false(oracleClient):
         )
 
 
-def test_insert_from_frame_connects_fix_club_true(oracleClient):
+def test_insert_from_frame_connects_fix_clob_true(oracle_client):
 
     with patch("muttlib.dbconn.base.create_engine") as create_engine:
 
         df = MagicMock(dtypes='object')
 
         table = "test_table"
-        oracleClient.insert_from_frame(df, table, fix_clobs=True)
+        oracle_client.insert_from_frame(df, table, fix_clobs=True)
         create_engine.assert_called_once_with(
-            oracleClient.conn_str, connect_args=ANY, echo=ANY
+            oracle_client.conn_str, connect_args=ANY, echo=ANY
         )
         df.to_sql.assert_called_once_with(
             table,
