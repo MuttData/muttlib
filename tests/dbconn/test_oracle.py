c3851dd87efc24fe4caab307c64e9da0422af962
Diego Lizondo <lizondo.diego@gmail.com>
2021-02-24 15:25:10 -0300

    Added test to upper_case_cols True and False.

--- tests/dbconn/test_oracle.py
+++ tests/dbconn/test_oracle.py
@@ -1,5 +1,8 @@
 from unittest.mock import patch, MagicMock, ANY
 
+import pandas as pd
+from pandas._testing import assert_frame_equal
+
 from muttlib.dbconn import OracleClient
 import pytest
 
@@ -90,3 +93,29 @@ def test_insert_from_frame_connects_fix_clobs_true(oracle_client):
             dtype={'col1': varchar(3), 'col2': varchar(3)},
             index=False,
         )
+
+
+def test_insert_from_frame_connects_upper_case_cols_true(oracle_client):
+
+    with patch("muttlib.dbconn.base.create_engine") as create_engine:
+
+        df = pd.DataFrame({'col1': [1], 'col2': [3.0]})
+
+        table = "test_table"
+        oracle_client.insert_from_frame(df, table, fix_clobs=True, upper_case_cols=True)
+
+        assert_frame_equal(df, pd.DataFrame({'COL1': [1], 'COL2': [3.0]}))
+
+
+def test_insert_from_frame_connects_upper_case_cols_false(oracle_client):
+
+    with patch("muttlib.dbconn.base.create_engine") as create_engine:
+
+        df = pd.DataFrame({'col1': [1], 'col2': [3.0]})
+
+        table = "test_table"
+        oracle_client.insert_from_frame(
+            df, table, fix_clobs=True, upper_case_cols=False
+        )
+
+        assert_frame_equal(df, pd.DataFrame({'col1': [1], 'col2': [3.0]}))
