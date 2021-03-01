3ebd4bcf6208c43e631a8a6d12f0891836f32f46
Fabian Wolfmann <fabian@muttdata.ai>
2021-03-01 21:27:43 +0000

    RFC: Add BigQuery functionality

--- tests/dbconn/test_bigquery.py
+++ tests/dbconn/test_bigquery.py
@@ -23,12 +23,12 @@ def df():
 
 
 def test_bigquery_close(dummy_db_credentials):
-    with patch("google.cloud.bigquery.client.Client") as client, patch(
+    with patch("google.cloud.bigquery.client.Client") as connection, patch(
         "google.cloud.bigquery.client.Client.close"
     ) as bq_close:
         bq_cli = BigQueryClient(**dummy_db_credentials)
         bq_cli.credentials = True
-        bq_cli.client = client
+        bq_cli.connection = connection
         bq_cli.execute("SELECT *")
 
         assert bq_close.call_count == 1
@@ -37,12 +37,12 @@ def test_bigquery_close(dummy_db_credentials):
 
 
 def test_execute(dummy_db_credentials):
-    with patch("google.cloud.bigquery.client.Client") as client, patch(
+    with patch("google.cloud.bigquery.client.Client") as connection, patch(
         "google.cloud.bigquery.client.Client.query"
     ) as bq_query:
         bq_cli = BigQueryClient(**dummy_db_credentials)
         bq_cli.credentials = True
-        bq_cli.client = client
+        bq_cli.connection = connection
 
         assert bq_query.call_count == 0
         bq_cli.execute("SELECT *")
@@ -50,13 +50,13 @@ def test_execute(dummy_db_credentials):
 
 
 def test_to_frame(dummy_db_credentials):
-    with patch("google.cloud.bigquery.client.Client") as client, patch(
+    with patch("google.cloud.bigquery.client.Client") as connection, patch(
         "google.cloud.bigquery.client.Client.query"
     ) as bq_query:
 
         bq_cli = BigQueryClient(**dummy_db_credentials)
         bq_cli.credentials = True
-        bq_cli.client = client
+        bq_cli.connection = connection
 
         assert bq_query.call_count == 0
         _ = bq_cli.to_frame("SELECT *")
@@ -64,12 +64,12 @@ def test_to_frame(dummy_db_credentials):
 
 
 def test_insert_from_frame(dummy_db_credentials, df):
-    with patch("google.cloud.bigquery.client.Client") as client, patch(
+    with patch("google.cloud.bigquery.client.Client") as connection, patch(
         "google.cloud.bigquery.client.Client.load_table_from_dataframe"
     ) as bq_load:
         bq_cli = BigQueryClient(**dummy_db_credentials)
         bq_cli.credentials = True
-        bq_cli.client = client
+        bq_cli.connection = connection
 
         assert bq_load.call_count == 0
         bq_cli.insert_from_frame(df, create_first=False)
