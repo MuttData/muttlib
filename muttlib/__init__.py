38a562357ee950e806c8a583027dcc217296c54a
CrossNox <ijmermet@gmail.com>
2022-01-18 15:44:41 -0300

    version check

--- muttlib/__init__.py
+++ muttlib/__init__.py
@@ -1,2 +1,7 @@
 """Version."""
-__version__ = '1.4.6'
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
+__version__ = importlib_metadata.version(__name__)  # type: ignore
