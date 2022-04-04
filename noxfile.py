bb82568e5c8fe27e35fd6328abe8775b7412055a
Agustin Dye <agustin.dye@muttdata.ai>
2022-04-04 14:33:25 -0300

    fix: update nox

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox_poetry as nox
 @nox.session(reuse_venv=True, python=["3.8", "3.9"])
 def tests(session):
     """Run all tests."""
-    # session.run_always("poetry", "install", "-E", "all", "-vvv", external=True)
+    session.run_always("poetry", "install", "-E", "all", "-vv", external=True)
     cmd = ["poetry", "run", "pytest", "-n", "auto", "--mpl"]
     if session.posargs:
         cmd.extend(session.posargs)
@@ -15,7 +15,7 @@ def tests(session):
 @nox.session(reuse_venv=True, python=["3.8", "3.9"])
 def precommit_hooks(session):
     """Run all pre-commit hooks."""
-    # session.run_always("poetry", "install", "-E", "all", "-vvv", external=True)
+    session.run_always("poetry", "install", "-E", "all", "-vv", external=True)
     session.run("pre-commit", "install")
     session.run("pre-commit", "run", "--show-diff-on-failure", "--all-files")
 
@@ -23,5 +23,5 @@ def precommit_hooks(session):
 @nox.session(reuse_venv=True, python=["3.8", "3.9"])
 def bandit(session):
     """Run all pre-commit hooks."""
-    # session.run_always("poetry", "install", "-E", "all", "-vvv", external=True)
+    session.run_always("poetry", "install", "-E", "all", "-vv", external=True)
     session.run("bandit", "-r", "muttlib/", "-ll", "-c", "bandit.yaml")
